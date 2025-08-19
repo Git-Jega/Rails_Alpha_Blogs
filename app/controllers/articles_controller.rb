10b816e769afe659271c808f24cc36c1d1b979b5
Jegadeeswar Rameswaran <jegadeeswar.rameswaran@bounteous.com>
2025-08-19 10:47:54 +0530

    added flash messaging and edit feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,10 +22,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title,:description))
     if @article.save
+      flash[:notice] = "Article was created successfully."
       # full form
       # redirect_to article_path(@article)
       # shortcut
-      flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
       render 'new'
