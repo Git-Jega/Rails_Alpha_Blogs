601566055cd9c11a123508c4bd551049b781850a
Jegadeeswar Rameswaran <jegadeeswar.rameswaran@bounteous.com>
2025-08-25 15:25:34 +0530

    code cleanup and updates

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,11 @@
-# Pin npm packages by running ./bin/importmap
+# config/importmap.rb
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Add these two lines for Bootstrap and Popper
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
