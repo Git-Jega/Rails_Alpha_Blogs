a4de15523b6ef04956e6735fa4689d3ac8501061
Jegadeeswar Rameswaran <jegadeeswar.rameswaran@bounteous.com>
2025-09-02 14:34:53 +0530

    Add one to ma y association between user and articles in BE

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_02_062332) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_02_085310) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
